8e29cc04d3c90cf788f99ddfaf810bf7bd2f0f78
Javier de la Rosa <versae@gmail.com>
2013-08-25 14:59:57 -0400

    Fixes requirements and DEBUG

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ from flask import Flask
 from flask.ext import restful
 from flask.ext.restful import reqparse
 
-DEBUG = bool(os.environ.get("DEBUG", True))
+DEBUG = bool(os.environ.get("DEBUG", False))
 
 app = Flask(__name__)
 api = restful.Api(app)
